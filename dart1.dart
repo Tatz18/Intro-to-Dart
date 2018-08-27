d58d33d3bb0cf91d811fbd2dc2b37aa27ca8cccf
ATRI DAS <babinsamrat123@gmail.com>
2018-08-27 20:30:02 +0530

    Update dart1.dart

--- dart1.dart
+++ dart1.dart
@@ -12,5 +12,20 @@ void main() {
       continue mylength;
     }
   }
+  
+  try {
+    deposite(-1000);
+  }catch (e){
+    print(e.error_message());
+  }
 }
 int perimeter(int length, int width) => length * width ;
+
+class DepositeException implements Exception{
+  String error_message() => "cant enter value less than 0";
+}
+void deposite (int amount){
+  if(amount < 0 ){
+    throw new DepositeException();
+  }
+}
